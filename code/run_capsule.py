5233c1f7871bdc793b8ed8c3139804a3b4e29e34
Rushil Chakrabarty <rushil.chakrabarty@alleninstitute.org>
2024-08-31 02:29:51 +0000

    Edited session_iter_example.ipynb, run_capsule.py, Dockerfile; and changes in 4 other files

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,24 @@
 """ top level run script """
+import os
+import subprocess
+
 
 def run():
     """ basic run function """
-    pass
+    session_ids = [1089296550, 1093642839, 1096620314, 1108334384, 1118324999, 1118327332, 1139846596]
+
+    for session_id in session_ids:
+        os.environ["session_id"] = str(session_id)
+        command = [
+                "jupyter",
+                "nbconvert",
+                "--execute",
+                "~/capsule/code/ephys_plotter.ipynb",
+                "--to",
+                "notebook",
+                "--output",
+                f"~/capsule/results/{session_id}_report.ipynb",
+            ]
+        subprocess.run(command, check=True)
 
 if __name__ == "__main__": run()
